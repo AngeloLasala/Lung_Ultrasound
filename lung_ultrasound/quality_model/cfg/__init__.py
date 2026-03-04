70d274c9876e7f3a00ddce973aa1ee688f54926f
Angelo Lasala <lasala.angelo96@gmail.com>
2026-03-04 15:02:04 +0100

    config file

---
+++ lung_ultrasound/quality_model/cfg/__init__.py
@@ -0,0 +1,55 @@
+class cfg:
+    """
+    Configuration for training the LUS clip classification model
+    """
+    seed = 42
+
+    ## folder configuration
+    main_path = "D:\Assistant_Researcher\Predict\OpenPOCUS"
+    dataset = "Extrapolates_frames"
+    results = "results"
+    save_folder = "checkpoints"
+    tensorboard_folder = "tensorboard"
+
+    ## dataset configuratios
+    size = 256
+    fold_cv = 'fold_1'        # cross-validation fold 
+    splitting = 'splitting_10_percent.json'
+
+    ## augumentation configuration (see dataset JointTrasformation for default value)
+    img_size = size
+    low_img_size = size
+    ori_size = size
+    crop = (32, 32)
+    p_flip = 0.5
+    p_rota = 0.5
+    p_scale = 0.5
+    p_gaussn = 0.0     # it doesn't work
+    p_contr = 0.5
+    p_gama = 0.5
+    p_distor = 0.0
+    color_jitter_params = None
+    p_random_affine = 0
+    long_mask = False
+
+    ## Training parameters    ##########################################################
+    workers = 1                         # number of data loading workers (default: 8)
+    epochs = 200                        # number of total epochs to run (default: 400)
+    batch_size = 8                      # batch size (default: 4)
+    learning_rate = 5e-4                # iniial learning rate (default: 0.001)
+    momentum = 0.9                      # momentum
+    num_classes = 3                     # then umber of classes (background + foreground)
+    img_size = 256                      # the input size of model
+    im_channels = 1
+    train_split = "train"               # the file name of training set
+    val_split = "val"                   # the file name of testing set
+    test_split = "test"                 # the file name of testing set
+    crop = None                         # the cropped image size
+    eval_freq = 1                       # the frequency of evaluate the model
+    save_freq = 2000                    # the frequency of saving the model
+    device = "cuda"                     # training device, cpu or cuda
+    cuda = "on"                         # switch on/off cuda option (default: off)
+    gray = "yes"                        # the type of input image
+    eval_mode = "mask_slice"            # the mode when evaluate the model, slice level or patient level
+    pre_trained = False
+    mode = "train"
