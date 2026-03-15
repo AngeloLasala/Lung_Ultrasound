0318f2ae3beea026ef3eba55f67a8a877c92d9e4
AngeloLasala <Angelo.Lasala@santannapisa.it>
2026-03-15 08:17:49 +0100

    update mutliple sbatch file

--- lung_ultrasound/quality_model/utils/bash_file/sbatch_train_5cv_2.sh
+++ lung_ultrasound/quality_model/utils/bash_file/sbatch_train_5cv_2.sh
@@ -5,15 +5,15 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=23:00:00              # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=train_5cv.err              # standard error file
-#SBATCH --output=train_5cv.out             # standard output file
+#SBATCH --error=train_5cv_2.err              # standard error file
+#SBATCH --output=train_5cv_2.out             # standard output file
 #SBATCH --account=IscrC_FouGenAI     # account name
 
 for fold in fold_1 fold_2 fold_3 fold_4 fold_5; do
     echo "Training $fold..."
     start=$(date +%s)
  
-    python -m lung_ultrasound.quality_model.tools.train --fold "$fold" --log info --keep_log
+    python -m lung_ultrasound.quality_model.tools.train --fold "$fold" --splitting "splitting_ext_plax.json" --log info --keep_log
  
     end=$(date +%s)
     elapsed=$(( end - start ))
